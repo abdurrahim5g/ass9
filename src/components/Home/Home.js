d7900db0f60eb06e630e60cf33ca233be659653d
f-rahim <freelancerrahim26@gmail.com>
2023-04-01 18:13:23 +0600

    Added SectionHeading component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,32 @@
 import React from "react";
 import AuthorHistory from "../AuthorHistory/AuthorHistory";
+import SectionHeading from "../SectionHeading/SectionHeading";
 import Welcome from "../Welcome/Welcome";
 
 const Home = () => {
   return (
-    <div className="hero-area py-12">
-      <div className="container ">
-        <div className="grid grid-cols-3 gap-8">
-          <div className="welcome-section ">
-            <Welcome></Welcome>
-          </div>
-          <div className="information-section col-span-2">
-            <AuthorHistory></AuthorHistory>
+    <>
+      <div className="hero-area py-12">
+        <div className="container ">
+          <div className="grid grid-cols-3 gap-8">
+            <div className="welcome-section ">
+              <Welcome></Welcome>
+            </div>
+            <div className="information-section col-span-2">
+              <AuthorHistory></AuthorHistory>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="quiz-section py-16 bg-slate-50">
+        <div className="container">
+          <SectionHeading
+            title="Trending Quizs"
+            subtitle={"Explore our"}
+          ></SectionHeading>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SectionHeading = ({ title, subtitle, color }) => {
+  return (
+    <div className="section-heading text-center">
+      <h3 className={`fancy-font text-2xl mb-2  ${color}`}>{subtitle}</h3>
+      <h3 className={`text-3xl font-semibold  ${color}`}>{title}</h3>
+    </div>
+  );
+};
+
+export default SectionHeading;
