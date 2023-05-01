7c8924d4f742a996a484da8c1385ad79f4e07bf1
f-rahim <freelancerrahim26@gmail.com>
2023-05-01 22:11:16 +0600

    Complete Navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,18 +11,18 @@ const Navbar = () => {
   ];
   return (
     <nav className="navbar bg-slate-100 py-2 shadow-md relative z-10">
-      <div className="container flex items-center justify-between">
+      <div className="container md:flex items-center justify-between">
         <div className="logo-area">
           <Link to={"/"}>
-            <img src={logo} alt="Quiz Pro" width={200} />
+            <img src={logo} alt="Quiz Pro" width={200} className="mx-auto" />
           </Link>
         </div>
-        <div className="menu-area">
+        <div className="menu-area mt-4 md:mt-0 flex justify-between">
           {menuItem.map((singleMenu, index) => (
             <NavLink
               to={`/${singleMenu.path}`}
               key={index}
-              className="text-gray-600"
+              className="text-gray-600 text-sm md:ml-3"
             >
               {singleMenu.name}
             </NavLink>
